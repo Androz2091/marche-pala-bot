f3a19fac4c0eada4aec4f69d96e95e333e9decb3
Androz2091 <androz2091@gmail.com>
2019-10-28 19:03:07 +0100

    Add helpcmd command

--- main.js
+++ main.js
@@ -27,6 +27,36 @@ client.on("message", (message) => {
         client.channels.get(config.cmdChannel).send(embed).then((m) => m.react(config.emoji));
         message.channel.send("Votre commande a été prise en compte, vous serez recontacté dans les plus brefs délais");
     }
+    if(command === "helpcmd"){
+        message.channel.send({ embed: {
+            title: "Aide pour les commandes du Shop",
+            color: 15158332,
+            fields: [
+                {
+                    value: "Faites la commande `​+commande`​ suivi de la quantité, et du nom de l'item choisi",
+                    name: "Étape 1:"
+                },
+                {
+                    value: "`​+commande 1 Full P4`​ commandera 1 Full P4",
+                    name: "Exemple :"
+                },
+                {
+                    value: "Attendez qu'un vendeur accepte votre commande. **Si vos mps sont ouverts**, vous recevrez une alerte automatiquement quand votre commande sera acceptée",
+                    name: "Étape 2"
+                },
+                {
+                    value: "Profitez de votre achat sur Paladium !",
+                    name: "Étape 3"
+                }
+            ],
+            thumbnail: {
+                url: "https://cdn.discordapp.com/avatars/280246799715926026/3008553069deead4f261e9bab520c6c1.web",
+                proxyURL: "https://images-ext-2.discordapp.net/external/pJQh7QvMU-DUqp1vZJ1DsZXw9zzV_vphFp-hjwTIE-k/https/cdn.discordapp.com/avatars/280246799715926026/3008553069deead4f261e9bab520c6c1.webp",
+                height: 128,
+                width: 128
+            }
+        }});
+    }
 });
 
 client.on("messageReactionAdd", (reaction, user) => {
